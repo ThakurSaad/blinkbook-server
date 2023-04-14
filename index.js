07e4ac22d90a9ea267e6387912a9f2086ea99d4b
ThakurSaad <thakursaad613@gmail.com>
2023-04-14 21:24:12 +0600

    login

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATION */
@@ -39,6 +40,9 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
