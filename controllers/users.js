667ac0c86a50cacd86abd9f464f7788433e00077
ThakurSaad <thakursaad613@gmail.com>
2023-04-18 21:57:07 +0600

    some fix

--- controllers/users.js
+++ controllers/users.js
@@ -35,17 +35,16 @@ export const getUserFriends = async (req, res) => {
 export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
-    const user = await User.findOne(id);
+    const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = user.friend.filter((id) => id !== id);
+      friend.friends = friend.friend.filter((id) => id !== id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
     }
-
     await user.save();
     await friend.save();
 
