eaad8df3c7b13a77f45c8beee616af4b1e619d4f
ThakurSaad <thakursaad613@gmail.com>
2023-04-19 12:51:57 +0600

    bug fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 import Post from "../models/Post.js";
 import User from "../models/User.js";
+import color from "colors";
 
 export const createPost = async (req, res) => {
   try {
@@ -40,7 +41,8 @@ export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
     const post = await Post.find({ userId });
-    res.status(200).json();
+
+    res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ error: err.message });
   }
--- controllers/users.js
+++ controllers/users.js
@@ -40,7 +40,7 @@ export const addRemoveFriend = async (req, res) => {
 
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friend.filter((id) => id !== id);
+      friend.friends = friend.friends.filter((id) => id !== id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
@@ -51,7 +51,6 @@ export const addRemoveFriend = async (req, res) => {
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     );
-
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
@@ -60,6 +59,6 @@ export const addRemoveFriend = async (req, res) => {
 
     res.status(200).json(formattedFriends);
   } catch (err) {
-    res.status(404).json({ error: err.message });
+    res.status(404).json({ message: err.message });
   }
 };
